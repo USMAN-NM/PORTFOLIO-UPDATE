d1880dcc34ce3603194e023422c35180af55ef20
Muhammad Usman <Muhammad.Usman@netmatters-scs.com>
2023-03-06 10:42:30 +0000

    New Commit

---
+++ JS/form.js
@@ -0,0 +1,41 @@
+function required()
+{
+var empt = document.form1.text1.value;
+if (empt === "")
+{
+alert("Please input a Value");
+return false;
+}
+else 
+{
+alert('Code has accepted : you can try another');
+return true; 
+}
+}
+
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
+
+
+
+var phone_input = document.getElementById("myform_phone");
+
+phone_input.addEventListener('input', () => {
+  phone_input.setCustomValidity('');
+  phone_input.checkValidity();
+});
+
+phone_input.addEventListener('invalid', () => {
+  if(phone_input.value === '') {
+    phone_input.setCustomValidity('Enter phone number!');
+  } else {
+    phone_input.setCustomValidity('Enter phone number in this format: 123-456-7890');
+  }
+});
